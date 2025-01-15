e6dd4d2eaee2605c3793232541d836629edd188e
Raphael Duarte <raphael-duarte@outlook.com>
2025-01-15 19:13:04 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Cliente.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula02.Entities
+{
+    public class Cliente
+    {
+        #region Atributos
+        private int idCliente;
+        private string nome;
+        private string email;
+        private Endereco endereco;
+        #endregion
+        #region Contrutores
+        //contrutor padrão
+        public Cliente()
+        {
+            
+        }
+        //sobrecarga de contrutor
+        public Cliente(int idCliente, string nome, string email)
+        {
+            this.idCliente = idCliente;
+            this.nome = nome;
+            this.email = email;
+        }
+        //sobre carga de construtor
+        public Cliente(int idCliente, string nome, string email, Endereco endereco)
+            : this(idCliente, nome, email)
+        {
+            this.endereco = endereco;
+        }
+        #endregion
+        #region Encapsulamento
+        public int IdCliente
+        {
+            set => idCliente = value; get => idCliente;
+        }
+        public string Nome
+        {
+            set => nome = value; get => nome;
+        }
+        public string Email
+        {
+            set => email = value; get => email;
+        }
+        public Endereco Endereco
+        {
+            set => endereco = value; get => endereco;
+        }
+        #endregion
+    }
+}
---
+++ Entities/Endereco.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula02.Entities
+{
+    public class Endereco
+    {
+        #region Atributos
+        private int idEndereco;
+        private string logradouro;
+        private string cidade;
+        private string estado;
+        private string cep;
+        #endregion
+        #region Construtores
+        //construtor default
+        public Endereco()
+        {
+            
+        }
+
+        //sobrecarga de construtor
+        public Endereco(int idEndereco, string logradouro, string cidade, string estado, string cep)
+        {
+            this.idEndereco = idEndereco;
+            this.logradouro = logradouro;
+            this.cidade = cidade;
+            this.estado = estado;
+            this.cep = cep;
+        }
+        #endregion
+        #region Encapsulamento
+        //public int IdEndereco
+        //{
+        //    set { idEndereco = value; } //entrada
+        //    get { return idEndereco; } //saída
+        //}
+        public int IdEndereco
+        {
+            set => idEndereco = value; get => idEndereco;
+        }
+        public string Logradouro
+        {
+            set { logradouro = value; } //entrada
+            get { return logradouro; } //saída
+        }
+        public string Cidade
+        {
+            set { cidade = value; } //entrada
+            get { return cidade; } //saída
+        }
+        public string Estado
+        {
+            set { estado = value; } //entrada
+            get { return estado; } //saída
+        }
+        public string Cep
+        {
+            set { cep = value; } //entrada
+            get { return cep; } //saída
+        }
+
+        #endregion
+    }
+}
